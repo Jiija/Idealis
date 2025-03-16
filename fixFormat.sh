645d50e3d476efaf45c1ebd13c24598085bef80a
Jiija <avarisov@yandex.ru>
2025-03-17 01:31:41 +0200

    Fix the formatting script

--- fixFormat.sh
+++ fixFormat.sh
@@ -1,1 +1,1 @@
-find . -path "./excluded_folder" -prune -o -name "*.cpp" -o -name "*.hpp" | xargs clang-format -i -style=file
+find . -path "./external/*" -prune -o \( -name "*.cpp" -o -name "*.hpp" \) -print | xargs clang-format -i -style=file
